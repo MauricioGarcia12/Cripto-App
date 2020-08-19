e4a3e8397e7ab40d029bfbcfff5d21e39482ee84
maumainsaw <maumainsaw@gmail.com>
2020-08-18 23:01:50 -0500

    Otorgando estilos al hook

--- src/hooks/useMoneda.jsx
+++ src/hooks/useMoneda.jsx
@@ -1,4 +1,26 @@
 import React, {Fragment, useState} from 'react'
+import styled from '@emotion/styled'
+
+const Label =styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color:white;
+    text-transform:uppercase;
+    font-weight:bold;
+    font-size:2.4rem;
+    margin-top:2rem;
+    display:block;
+`;
+
+const Select =styled.select`
+    width:100%;
+    display:block;
+    padding:1rem;
+    --webkit-appearance:none;
+    border:none;
+    border-radius:10px;
+    font-size:1.2rem;
+`;
+
 
 const useMoneda = (label,stateInicial,opciones) => {
     //sTATE DE NUESTRO CUSTOM HOOK
@@ -7,8 +29,8 @@ const useMoneda = (label,stateInicial,opciones) => {
     //Lo que se muestra en pantalla
     const Seleccionar = () => (
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
                 onChange={e => actualizarState(e.target.value)}
                 value={state}
             >
@@ -18,7 +40,7 @@ const useMoneda = (label,stateInicial,opciones) => {
                         {opcion.nombre}
                     </option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
     )
 
